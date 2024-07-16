f799d24ae1afb14e375f7bfb59542c41b9ffa377
Кирилл <masteryoda01010101@gmail.com>
2024-07-16 16:20:36 +0300

    second get

--- main.py
+++ main.py
@@ -9,7 +9,10 @@ bd = [
     {"id": 2, "role": "leader", "name": "Alex"},
     {"id": 3, "role": "user", "name": "Alan"},
 ]
-@app.get("/users")
+@app.get("/users/{user_id}")
 async def get_user_id(user_id: int):
-    return {"Hello": "World"}
+    return [user for user in bd if user.get("id") == user_id]
 
+@app.get("/name_users/{user_id}")
+async def get_name_id(name_id: str):
+    return [user for user in bd if user.get("name") == name_id]
