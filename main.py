ec8f71c73f4ef040d3baa36e30989a649b363ce7
Кирилл <masteryoda01010101@gmail.com>
2024-07-16 16:08:35 +0300

    new local data

--- main.py
+++ main.py
@@ -1,8 +1,15 @@
 from fastapi import FastAPI
 
-app = FastAPI()
+app = FastAPI(
+    title="App"
+)
 
+bd = [
+    {"id": 1, "role": "admin", "name": "Alma"},
+    {"id": 2, "role": "leader", "name": "Alex"},
+    {"id": 3, "role": "user", "name": "Alan"},
+]
 @app.get("/users")
-async def hello():
+async def get_user_id(user_id: int):
     return {"Hello": "World"}
 
