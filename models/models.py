b8719a7080a64abb55dac2b58efdc07316f0e73c
Кирилл <masteryoda01010101@gmail.com>
2024-07-19 16:37:35 +0300

    new models

---
+++ models/models.py
@@ -0,0 +1,26 @@
+from sqlalchemy import MetaData, JSON, Table, Column, Integer, String, TIMESTAMP, ForeignKey
+
+from datetime import datetime
+
+
+metadata = MetaData()
+
+roles = Table(
+    "roles",
+    metadata,
+    Column("id", Integer, primary_key=True),
+    Column("name", String, nullable=False),
+    Column("permissions", JSON),
+)
+users = Table(
+    "users",
+    metadata,
+    Column("id", Integer, primary_key=True),
+    Column("email", String, primary_key=False),
+    Column("username", String, primary_key=False),
+    Column("password", String, nullable=False),
+    Column("registered_at", TIMESTAMP , default=datetime.utcnow),
+    Column(""),
+
+
+)
